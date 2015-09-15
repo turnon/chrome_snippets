bf0df23d8cff161dcd825068b6dffca2ff9cdc73
ken <block24block@gmail.com>
2015-09-15 20:45:47 +0800

    also show year and month

--- saltt.js
+++ saltt.js
@@ -4,13 +4,17 @@
     var kw = Object.create(null);
     
     // put into hash
-    $('.car-monthlisting li a').each(function each_li(){
-        var bookname = $(this).text();
-        bookname.split(/\s/).forEach(function into_hash(e){
-            var pure_kw = e.replace(/\W/,'').toLowerCase();
-            if(kw[pure_kw] === undefined) kw[pure_kw] = [];
-            kw[pure_kw].push(bookname);
-        });        
+    $('.car-list > li').each(function each_ym(){
+        var $ym_li = $(this);
+        var ym = $ym_li.find('.car-yearmonth').text().replace(/\(.*/,'').replace(/\s/g,'');
+        $ym_li.find('ul li a').each(function each_li(){
+            var bookname = $(this).text();
+            bookname.split(/\s/).forEach(function into_hash(e){
+                var pure_kw = e.replace(/\W/,'').toLowerCase();
+                if(kw[pure_kw] === undefined) kw[pure_kw] = [];
+                kw[pure_kw].push(bookname + ' => ' + ym);
+            });
+        });
     });
     
     // cache keyword list
