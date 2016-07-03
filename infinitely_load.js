a9897c7dc085c861e48d10295ddcbdf20dbab85d
ken <block24block@gmail.com>
2016-07-03 11:44:10 +0800

    merged from my chrome extendsion

--- infinitely_load.js
+++ infinitely_load.js
@@ -1,62 +1,75 @@
-function selectors() {
+(function(){
 
-    var supported = {
-        gamersky: [".page_css b", ".Mid2L_con,.MidL_con"],
-        wallpaperscraft: [".page_active", ".center", 10],
-        yinyuetai: [function(doc){return $(doc).find(".page-nav span").filter(function(){return $(this).attr('class') == null})}, ".mv_list,.page-nav"],
-        "tieba\.baidu\.com\/p": ['.pb_footer .tP', '.pb_content,.pb_footer'],
-        ituring: ['.PagedList-currentPage', '#mainbar', 5],
-        "epubit\.com\.cn\/book": ['.PagedList-currentPage', '.col-md-9 .panel', 5],
-    }
-
-    var url = window.location.href;
-    var selector = null;
-
-    for (var pattern in supported) {
-        var re = new RegExp(pattern);
-        if (url.match(re)) {
-            selector = supported[pattern];
-            console.log(pattern, selector);
-            break;
+    function selectors() {
+        var url = window.location.href;
+        var selector = null;
+        for (var pattern in supported) {
+            var re = new RegExp(pattern);
+            if (url.match(re)) {
+                selector = supported[pattern];
+                console.log(pattern, selector);
+                return selector;
+                break;
+            }
         }
+        console.log("not supported!");
     }
 
-    return selector;
-}
+    function work_with($){
 
-function loadp(cur_p, content, limit){
+        function loadp(cur_p, content, limit){
 
-    var $tail = $(content).last();
+            var $tail = $(content).last();
 
-    limit = (limit || 99);
+            limit = (limit || 99);
 
-    function next_link(doc){
-        if(--limit == 0) return;
-        var $nex_p = ((typeof cur_p == "function") ? cur_p(doc) : $(doc).find(cur_p)).next();
-        var link = ($nex_p.is('a') ? $nex_p : $nex_p.find('a')).attr('href');
-        console.log(link);
-        return link;
+            function next_link(doc){
+                if(--limit == 0) return;
+                var $nex_p = ((typeof cur_p == "function") ? cur_p(doc) : $(doc).find(cur_p)).next();
+                var link = ($nex_p.is('a') ? $nex_p : $nex_p.find('a')).attr('href');
+                console.log(link);
+                return link;
+            }
+
+            function process_page(data){
+                var $cnt = $(data).find(content);
+                console.log($cnt, $cnt.length);
+                $tail.after($cnt);
+                $tail = $cnt.last();
+                _loadp(next_link(data));
+            }
+
+            function _loadp(link){
+                link && $.get(link, process_page);
+            }
+
+            _loadp(next_link(document));
+
+        }
+
+        var slt = selectors();
+        slt && loadp.apply(null, slt);
+
     }
 
-    function process_page(data){
-        var $cnt = $(data).find(content);
-        console.log($cnt);
-        $tail.after($cnt);
-        $tail = $cnt.last();
-        _loadp(next_link(data));
+    function afterJqLoaded(){
+        o_j = window.jQuery.noConflict();
+        work_with(o_j);
     }
 
-    function _loadp(link){
-        link && $.get(link, process_page);
+    function loadJq(){
+        var cdn = 'https://code.jquery.com/jquery-1.8.3.min.js';
+        var fileref=document.createElement('script');
+        fileref.setAttribute("type","text/javascript");
+        fileref.setAttribute("src", cdn);
+        fileref.onload = afterJqLoaded;
+        document.getElementsByTagName("head")[0].appendChild(fileref);
     }
 
-    _loadp(next_link(document));
+    function noJq(){
+        return typeof jQuery === 'undefined' || !jQuery.fn.jquery.match(/1\.8/);
+    }
 
-}
+    noJq() ? loadJq() : work_with(jQuery);
 
-var slt = selectors();
-
-slt && $(document).ready(function(){
-    console.log('ready');
-    loadp.apply(null, slt);
-});
+})();
